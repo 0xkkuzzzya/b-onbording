8d8cc4c322c38c7251c13c199de5291c1c290c27
0xkkuzzzya <130092860+0xkkuzzzya@users.noreply.github.com>
2024-08-20 17:18:37 +0500

    test:

--- src/components/Page/helpers/en-interface.tsx
+++ src/components/Page/helpers/en-interface.tsx
@@ -12,7 +12,6 @@ import Copy from '../../../assets/Copy.png'
 
 const Container = styled.div`
     width: 80%;
-    height: 100vh;
     display: flex;
     flex-direction: column;
     align-items: center;
