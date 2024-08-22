f528619ef1f0d971b9dc7e20386850038c73d12d
0xkkuzzzya <130092860+0xkkuzzzya@users.noreply.github.com>
2024-08-23 01:03:48 +0500

    test:

--- src/components/Page/MainPage/MainPageStoriesRU.tsx
+++ src/components/Page/MainPage/MainPageStoriesRU.tsx
@@ -1,36 +1,51 @@
 import styled from "styled-components";
 import Stories from 'react-insta-stories';
 import firstStories from '../../../assets/Stories/FirstStoriesRU.png'
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 
 
 export const StoriesPageRU = () => {
 
-	const navigate = useNavigate();
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const navigate = useNavigate();
 
-	useEffect(() => {
-		window.Telegram.WebApp.BackButton.show()
+
+    useEffect(() => {
+        window.Telegram.WebApp.BackButton.show()
         window.Telegram.WebApp.MainButton.show()
         window.Telegram.WebApp.BackButton.onClick(() => navigate(-1))
-	}, [])
+        window.Telegram.WebApp.MainButton.onClick(() => handleNext())
+    }, [])
 
-	const stories = [
-		{
-			url: firstStories,
-			duration: 5000,
-		},
-		{
-			url: "https://avatars.mds.yandex.net/i?id=2d29f4839ac021807aae0f05cdbf6231ddc92a2ab1848bbf-11540573-images-thumbs&n=13",
-			duration: 5000,
-		},
-	];
+    const stories = [
+        {
+            url: firstStories,
+            duration: 5000,
+        },
+        {
+            url: "https://avatars.mds.yandex.net/i?id=2d29f4839ac021807aae0f05cdbf6231ddc92a2ab1848bbf-11540573-images-thumbs&n=13",
+            duration: 5000,
+        },
+    ];
 
-	return (
-		<Stories
-			stories={stories}
-			width="100%"
-			height="100vh"
-		/>
-	);
+    const handleNext = () => {
+        setCurrentIndex((prevIndex) => (prevIndex < stories.length - 1 ? prevIndex + 1 : prevIndex));
+    };
+
+
+    return (
+        <Stories
+            stories={stories}
+            currentIndex={currentIndex}
+            onStoryEnd={(index: number, story: any) => {
+                if (index < stories.length - 1) {
+                    setCurrentIndex(index + 1);
+                }
+            }}
+            onAllStoriesEnd={() => navigate(-1)}
+            width="100%"
+            height="100vh"
+        />
+    );
 } 
